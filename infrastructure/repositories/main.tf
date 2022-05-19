fa8237e762ed4a814dea90e3127d3230d76b6229
tshepolepono <leponotshepo@gmail.com>
2022-05-19 13:49:39 +0200

    add changes

--- infrastructure/repositories/main.tf
+++ infrastructure/repositories/main.tf
@@ -9,7 +9,15 @@
 /**
 * The ECR repository we'll push our images to.
 */
+/**
+*  Add scan on push to see image vulnerabilities
+*/
+
 resource "aws_ecr_repository" "ceros_ski" {
   name = "ceros-ski"
   image_tag_mutability = "MUTABLE"
+
+   image_scanning_configuration {
+    scan_on_push = true
+  }
 }
