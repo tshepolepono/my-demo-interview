f30f4b8a416b5f303244da28ae6ae56807dccb13
tshepolepono <leponotshepo@gmail.com>
2022-05-22 12:22:22 +0000

    remove node modules

--- node_modules/ceros-ski/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(port);
