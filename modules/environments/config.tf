b6c762732c9ed1eb4bda82934fbfc3a4cf6e63d4
tshepolepono <leponotshepo@gmail.com>
2022-05-22 17:46:55 +0200

    update files

--- modules/environments/config.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region 
-  shared_credentials_file = var.aws_credentials_file 
-  profile = var.aws_profile 
-}
-
-terraform {
-  required_version = ">= 0.14.4"
-}
--- modules/environments/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "url" {
-  value = aws_lb.ecs.dns_name
-}
